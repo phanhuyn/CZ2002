4ad612d2a984b4cb0e90a3166fc8bde8afd25f73
Zillion-Govin <zillion.govin@gmail.com>
2014-11-09 00:12:34 +0800

    Update OrderController.java

--- src/Controller/OrderController.java
+++ src/Controller/OrderController.java
@@ -9,15 +9,46 @@ import java.util.ArrayList;
 import Entity.MenuItem;
 import Entity.Order;
 import Entity.PromotionalPackage;
+import UI.OrderUI;
 
 public class OrderController {
 	private ArrayList<Order> listOrder;
+	private OrderUI mOrderUI;
+	
 	public OrderController(){
-		listOrder = Order.mOrderList;
+		listOrder = Order.mOrderList
 	}
-	/*
-	 * Constructor
-	 */
+	
+	
+	 
+	public void run() {
+		int choice = 0;
+		while (choice <= 5){
+			System.out.println("1. Create Order");
+			System.out.println("2. View Order.");
+			System.out.println("3. Print Invoice.");
+			System.out.println("4. Print Sales Revenue Report by Date.");
+			System.out.println("5. Print Sales Revenue Report by Month.");
+			System.out.println("6. Quit.");
+			
+			System.out.println("Select your option: ");
+			
+			Scanner sc = new Scanner(System.in);
+			choice = sc.nextInt();
+			
+			switch (choice) {
+			case 1: break;
+			case 2: break;
+			case 3:	//printInvoice();
+				break;
+			case 4: break;
+			case 5: break;
+			case 6: break;
+			}
+		}
+		return;
+	} 
+	
 	public Order find(String customerName, int mId) {
 		// TODO Auto-generated method stub
 		for(Order order: listOrder){
