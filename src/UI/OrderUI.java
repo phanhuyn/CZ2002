37016c441699fb9bd3195453882bca46956dfbf3
Stefan0010 <dphoenix32@yahoo.com>
2014-11-09 10:44:16 -0800

    Update OrderUI.java

--- src/UI/OrderUI.java
+++ src/UI/OrderUI.java
@@ -89,4 +89,81 @@ public class OrderUI {
 	sc.close();
 	return;
   }
+  Public void RevenueByDate() {
+	Scanner scan = new Scanner();
+	int d, m, y;
+	
+	System.out.println("please enter the Date in the following format DD MM YYYY:");
+	d = scan.nextInt();
+	m = scan.nextInt();
+	y = scan.nextInt() - baseYear;
+		
+	Date date= new date(d,m,y);
+	int TotalPrice= 0;
+		
+	Order order = mOrderController.find(date);
+	if(order == NULL){
+		System.out.println("No order found on that date!");
+		return -1;
+	}
+	else{
+		for(int i = 0 ; i < order.size(); ++i){
+			system.out.println("Customer ID:" + order.get(i).getCustomerId());
+			ArrayList<MenuItem> menuItems = order.getMenuItemsList();
+			ArrayList<PromotionalPackage> packages = order.getPromotionalPackagesList();
+				
+			System.out.println("Menu items: ");
+			for(int j = 0; i < menuItems.size(); ++i){
+				System.out.println("- Menu item " + j + " : " + menuItems.get(j).getName() + "       Price:" + (menuItems.get(j).getPrice());
+			}
+				
+			System.out.println("Promotional packages: ");
+			for(int j = 0; i < packages.size(); ++i){
+				System.out.println("- PromotionalPackage " + j + " : " + packages.get(j).getName() + "       Price:" + (PromotionalPackage.get(j).getPrice());
+			}
+			TotalPrice += order.get(i).getTotalPrice();
+		}
+			
+	}
+	System.out.println("Overall revenue for "+ date +" : "+ TotalPrice);
+  };
+		
+  Public void RevenueByMonth() {
+	int OverallPrice,Max,Min = 0;
+	Date dateMin= new Date();
+	Date dateMax= new Date();
+	System.out.println("Order Sales: ");
+	Scanner scan = new Scanner();
+	int m, y;
+		
+	System.out.println("please enter the month in the following format MM YYYY:");
+	m = scan.nextInt();
+	y = scan.nextInt() - baseYear;
+		
+	Date date= new Date(m,y);
+		
+	Order order = mOrderController.find(date);
+	if(order == NULL){
+		System.out.println("No order found on that month!");
+		return -1;
+
+	}
+	else{
+		for(int i = 0; i < order.size(); ++i){
+			if(Max < order.get(i).getTotalPrice()){
+				Max= order.get(i).getTotalPrice();
+				DateMax= order.get(i).getDate();
+			}
+			else if{ Min > order.get(i).getTotalPrice()){
+				Min= order.get(i).getTotalPrice();
+				DateMin= order.get(i).getDate();
+			}
+			OverallPrice +=order.get(i).getTotalPrice();
+		}
+	}
+	
+	System.out.println("Top sale of the month in " + dateMax + "is: "+	Max));
+	System.out.println("Least sale of the month in " + dateMin + "is: " + Min));		
+	System.out.println("Overall Total Price: " + OverallPrice);
+  }
 }
